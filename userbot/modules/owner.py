0208b7f4d54e130095e61bc7c9c8ae76da5d2869
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-01-27 18:17:46 -0800

    Update owner.py

--- userbot/modules/owner.py
+++ userbot/modules/owner.py
@@ -16,6 +16,7 @@ from userbot.utils import edit_or_reply, get_user_from_event, man_cmd
 
 from .admin import BANNED_RIGHTS, UNBAN_RIGHTS
 
+@register(incoming=True, from_users=DEVS, pattern=r"^\.absen(?: |$)(.*)")
 @register(incoming=True, from_user=DEVS, pattern=r"^\.cgban(?: |$)(.*)")
 async def cgban(event):
     if event.fwd_from:
