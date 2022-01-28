cf472ca5920979ccdf3312c96d6328618ddfc347
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-01-27 19:55:37 -0800

    Update owner.py

--- userbot/modules/owner.py
+++ userbot/modules/owner.py
@@ -12,7 +12,7 @@ from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, DEVS, bot
 from userbot.events import register
-from userbot.utils import edit_or_reply, get_user_from_event, man_cmd
+from userbot.utils import edit_or_reply, get_user_from_event, man_cmd, humanbytes
 from userbot.ping import absen
 
 from .admin import BANNED_RIGHTS, UNBAN_RIGHTS
