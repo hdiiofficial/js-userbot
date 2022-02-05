2b3671ee9564112af28db3d3efb6b9de50f4d1e2
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-02-05 01:34:12 +0100

    Create Data.py

---
+++ userbot/utils/Data.py
@@ -0,0 +1,11 @@
+class Data:
+    #gausah di ganti eror gabisa benerin ntar nanya2
+    #credit @hdiiofficial
+    #lu ganti credit,gw tandain lu
+    copyright = """
+QEpvd29fU3RvcmU=
+"""
+
+    credit = """
+QG9mZmljaWFsX2pzX3VzZXJib3Q=
+"""
